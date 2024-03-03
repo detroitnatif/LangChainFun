ce20c27626cef1e6804a2b985e022d3a07778b24
detroitnatif <tytydizzle124@gmail.com>
2024-03-03 09:22:21 +0100

    langchain/streamlit proj

---
+++ main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from streamlit_chat import message
+from dotenv import load_dotenv
+import os
+
+from langchain_community.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage,
+)
+
+
+
+def main():
+    st.set_page_config(
+        page_title="TylerGPT",
+        page_icon="♓︎",
+    
+    )
+
+    load_dotenv()
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        st.error("You need to set your OpenAI API key in a .env file")
+        return
+    else:
+        st.write("OpenAI API Key is set")
+
+
+    chat = ChatOpenAI()
+
+    messages = [
+        SystemMessage(content="You are a helpful assistant to answer questions about Tyler"),
+        HumanMessage(content=input),
+    ]
+
+
+    st.header("TylerGPT")
+    
+    message("Hello, I'm Tyler's Chatbot, Ask me anything about him!")
+    message("What are Tyler's hobbies?", is_user=True)
+    
+
+    with st.sidebar:
+        user_input = st.text_input("Ask away")
+
+
+
+if __name__ == "__main__":
+    main()
